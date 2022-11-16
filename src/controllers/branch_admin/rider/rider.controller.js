d4d88637b6236ce6a72ab9114d2a9461e75b9a46
bhoobalan2022 <110883166+bhoobalan2022@users.noreply.github.com>
2022-11-16 18:44:20 +0530

    styles and root and rider

--- src/controllers/branch_admin/rider/rider.controller.js
+++ src/controllers/branch_admin/rider/rider.controller.js
@@ -1,112 +1,26 @@
-import knex from "../../../services/db.service";
-
-export const updateCategory = async (req, res) => {
+export const getApprovePurchaseList = async (req, res) => {
   try {
-    const { name, id , product_type_id } = req.body;
-    const file = req.file;
-
-    if (!name) {
-      req.flash("error", "Name is missing");
-      return res.redirect("/super_admin/product/get_category");
-    }
-
-    let query = {};
-
-    query.name = name;
-    if (file) {
-      const image = req.file.destination.slice(1) + "/" + req.file.filename;
-
-      query.image = image;
-    }
-
-    if(product_type_id){
-        query.product_type_id = product_type_id
-    }
-
-    await knex("categories").update(query).where({ id: id });
-
-    req.flash("success", "Updated SuccessFully");
-    res.redirect("/super_admin/product/get_category");
+    res.render("branch_admin/ride/apporve_purchase_order");
   } catch (error) {
     console.log(error);
     res.redirect("/home");
   }
 };
 
-export const updateCategoryStatus = async (req, res) => {
+export const getGeneratePurchaseList = async (req, res) => {
   try {
-    const { status, id } = req.body;
 
-    if (status == "1") {
-      await knex("categories").update({ status: "0" }).where({ id: id });
-    } else {
-      await knex("categories").update({ status: "1" }).where({ id: id });
-    }
-
-    req.flash("success", "Updated SuccessFully");
-    res.redirect("/super_admin/product/get_category");
+   res.render("branch_admin/ride/generate-purchase_order");
   } catch (error) {
     console.log(error);
     res.redirect("/home");
   }
 };
 
-export const createCategory = async (req, res) => {
+export const getCancelPurchaseList = async (req, res) => {
   try {
-    const { name, product_type_id } = req.body;
-    if (!name) {
-      req.flash("error", "Name is missing");
-      return res.redirect("/super_admin/product/get_category");
-    }
-    if (!product_type_id) {
-      req.flash("error", "Please Choose a Product Type");
-      return res.redirect("/super_admin/product/get_category");
-    }
-
-    if (!req.file) {
-      req.flash("error", "Please Choose a image");
-      return res.redirect("/super_admin/product/get_category");
-    }
-
-    const image = req.file.destination.slice(1) + "/" + req.file.filename;
-
-    await knex("categories").insert({ name, product_type_id, image });
-
-    req.flash("success", "Successfully Created");
-    res.redirect("/super_admin/product/get_category");
-  } catch (error) {
-    console.log(error);
-    res.redirect("/home");
-  }
-};
-
-export const getCategory = async (req, res) => {
-  try {
-    const categories = await knex("categories")
-      .select(
-        "categories.id",
-        "categories.name",
-        "categories.image",
-        "categories.status",
-        "product_type.name as product_type",
-        "product_type.id as product_type_id"
-      )
-      .join(
-        "product_type",
-        "categories.product_type_id",
-        "=",
-        "product_type.id"
-      );
-
-    for (let i = 0; i < categories.length; i++) {
-      categories[i].image = "http://" + req.headers.host + categories[i].image;
-    }
-
-    const productType = await knex("product_type")
-      .select("name", "id")
-      .where({ status: "1" });
-
-    res.render("product/category", { data: categories, productType });
+    
+    res.render("branch_admin/ride/cancelled_purchase_order");
   } catch (error) {
     console.log(error);
     res.redirect("/home");
--- src/controllers/branch_admin/root/root.controller.js
+++ src/controllers/branch_admin/root/root.controller.js
@@ -1,46 +1,28 @@
-// import knex from "../../../services/db.service";
+export const getApprovePurchaseList = async (req, res) => {
+  try {
+    res.render("branch_admin/root/apporve_purchase_order");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/home");
+  }
+};
 
-// export const getAllPlaces = async (req, res) => {
-//     try {
-//       // const categories = await knex("categories").join('product_type','categories.id','=','product_type.id')
-//       // .select("id", "name")
-//       //   // .select("id", "name", "image", "status")
-//       //   .where({ status: "1" });
-  
-//       res.render("places/city");
-//     } catch (error) {
-//       console.log(error);
-//       res.redirect("/home");
-//     }
-//   };
+export const getGeneratePurchaseList = async (req, res) => {
+  try {
 
+   res.render("branch_admin/root/generate-purchase_order");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/home");
+  }
+};
 
-export const getAddOnProduct = async (req, res) => {
-    try {
-      // const categories = await knex("categories").join('product_type','categories.id','=','product_type.id')
-      // .select("id", "name")
-      //   // .select("id", "name", "image", "status")
-      //   .where({ status: "1" });
-  
-      res.render("reports/add_on_product");
-    } catch (error) {
-      console.log(error);
-      res.redirect("/home");
-    }
-  };
-
-  export const getSubscriptionProduct = async (req, res) => {
-    try {
-      // const categories = await knex("categories").join('product_type','categories.id','=','product_type.id')
-      // .select("id", "name")
-      //   // .select("id", "name", "image", "status")
-      //   .where({ status: "1" });
-  
-      res.render("reports/subscription");
-    } catch (error) {
-      console.log(error);
-      res.redirect("/home");
-    }
-  };
-
-  
+export const getCancelPurchaseList = async (req, res) => {
+  try {
+    
+    res.render("branch_admin/root/cancelled_purchase_order");
+  } catch (error) {
+    console.log(error);
+    res.redirect("/home");
+  }
+};
