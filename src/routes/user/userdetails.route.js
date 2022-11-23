1ad14cf98e3c83422154593565cf02e92fa7c95d
ahamed hussain <ahamedhussain.gts@gmail.com>
2022-11-23 11:06:42 +0530

    conflict fix 5

--- src/routes/user/userdetails.route.js
+++ src/routes/user/userdetails.route.js
@@ -1,13 +1,35 @@
 import express from 'express';
-import { addUserAddress } from '../../controllers/user/userDetail.controller';
+import { addUserAddress,getAddress,editAddress,getUser,updateUser,deleteUseraddress, } from '../../controllers/user/userDetail.controller';
 
 const userRouter = express.Router({
     caseSensitive: true,
     strict: true
   })
 
+  import multer from "multer";
 
-userRouter.post('/add_user_address',addUserAddress)
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+      },
+    filename: function (req, file, cb) {
+      // console.log(req.headers.authorization)
+      let index = file.mimetype.indexOf('/') + 1
+      cb(null, Date.now() + "." + file.mimetype.slice(index));
+    }
+  });
+  
+  const uploadImg = multer({storage: storage}).single('image');
+  
+  userRouter.post('/add_address',addUserAddress)
+  userRouter.get('/get_address',getAddress)
+  userRouter.post("/edit_address",editAddress)
+  userRouter.get('/get_users',getUser)
+  userRouter.post('/update_users',uploadImg,updateUser)
+  userRouter.get('/delete_useraddress',deleteUseraddress)
+  
+  
+
 
 
 
