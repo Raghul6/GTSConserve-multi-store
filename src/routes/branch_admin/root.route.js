676737a31095ca4922b13d8e138c836eb4fc22cb
bhoobalan2022 <110883166+bhoobalan2022@users.noreply.github.com>
2022-11-23 19:00:43 +0530

    root route

--- src/routes/branch_admin/root.route.js
+++ src/routes/branch_admin/root.route.js
@@ -1,13 +1,13 @@
 import express  from 'express';
 
-import { loginHandler,loginForm } from "../../controllers/branch_admin/root/root.controller"
+import { getRoot } from "../../controllers/branch_admin/root/root.controller"
 
 
 const rootRouter = express.Router({
   caseSensitive: true,
   strict: true
 })
-// rootRouter.get('/login',loginForm)
+rootRouter.get('/get_root',getRoot)
 // rootRouter.post('/login', loginHandler)
 
 
