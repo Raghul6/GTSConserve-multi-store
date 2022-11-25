ecb513eea4b0604412f6c3dcad6526eeb2a73b6e
ahamed hussain <ahamedhussain.gts@gmail.com>
2022-11-25 16:33:24 +0530

    merging2

--- src/routes/user/userdetails.route.js
+++ src/routes/user/userdetails.route.js
@@ -1,31 +1,13 @@
 import express from 'express';
-import { addUserAddress,getAddress,editAddress,getUser,updateUser,deleteUseraddress, } from '../../controllers/user/userDetail.controller';
+import { addUserAddress } from '../../controllers/user/userDetail.controller';
 
 const userRouter = express.Router({
     caseSensitive: true,
     strict: true
   })
 
-  import multer from "multer";
 
-  const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
-      },
-      filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname);
-     }
-  });
-  
-  const uploadImg = multer({storage: storage}).single('image');
-  
-  // userRouter.post('/add_address',addUserAddress)
-  userRouter.get('/get_address',getAddress)
-  userRouter.post("/edit_address",editAddress)
-  userRouter.get('/get_users',getUser)
-  userRouter.post('/update_users',uploadImg,updateUser)
-  userRouter.get('/delete_useraddress',deleteUseraddress)
-  userRouter.post('/add_user_address',addUserAddress)
+userRouter.post('/add_user_address',addUserAddress)
 
 
 
