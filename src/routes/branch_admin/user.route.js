01741e2a80030721c2f4e16ff19619d1c0ed4ffb
ahamed hussain <ahamedhussain.gts@gmail.com>
2022-12-06 15:19:44 +0530

    branch admin user folder created

---
+++ src/routes/branch_admin/user.route.js
@@ -0,0 +1,16 @@
+import express  from 'express';
+
+
+
+const userRouter = express.Router({
+  caseSensitive: true,
+  strict: true
+})
+// userRouter.get('/get_route',getRoute)
+
+
+
+
+
+
+export default userRouter
