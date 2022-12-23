682961be9012abf69bf146621ab435a6e8ead538
Raghul6 <raghul03.gtech@gmail.com>
2022-12-23 17:01:07 +0530

    new rider route

--- src/routes/rider/rider.route.js
+++ src/routes/rider/rider.route.js
@@ -2,27 +2,27 @@ import express from 'express';
 import { getAppControls, getRiderdetails, getSingleorder, login, orderStatusUpdate, riderDashboard, updateEndtour, updateRiderstatus,updateStartTour,updeteRiderLocation } from "../../controllers/rider/rider.controller"
 import { nonMandatoryToken } from '../../middlewares/authToken.middleware';
 
-const loginRouter = express.Router({
+const riderRouter = express.Router({
     caseSensitive: true,
     strict: true,
 });
-loginRouter.get("/app_controls", getAppControls);
-loginRouter.post("/login",nonMandatoryToken,login)
-loginRouter.post("/rider_details",getRiderdetails)
+// riderRouter.get("/app_controls", getAppControls);
+// riderRouter.post("/login",nonMandatoryToken,login)
+riderRouter.post("/rider_details",getRiderdetails)
 
 
-loginRouter.post("/update_rider_status",updateRiderstatus);
-loginRouter.post("/update_rider_location",updeteRiderLocation);
-loginRouter.post("/update_start_tour",updateStartTour);
-loginRouter.post("/update_end_tour",updateEndtour);
+riderRouter.post("/update_rider_status",updateRiderstatus);
+riderRouter.post("/update_rider_location",updeteRiderLocation);
+riderRouter.post("/update_start_tour",updateStartTour);
+riderRouter.post("/update_end_tour",updateEndtour);
 
 
-loginRouter.post("/get_single_order",getSingleorder);
-loginRouter.post("/order_status_update",orderStatusUpdate);
-loginRouter.post("/rider_dashboard",riderDashboard);
+riderRouter.post("/get_single_order",getSingleorder);
+riderRouter.post("/order_status_update",orderStatusUpdate);
+riderRouter.post("/rider_dashboard",riderDashboard);
 
 
 
 
 
-export default loginRouter
+export default riderRouter
