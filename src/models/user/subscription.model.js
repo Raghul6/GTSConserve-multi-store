fc86a2ea074f6a60404372c0199e7398fc373347
bhoobalan2022 <110883166+bhoobalan2022@users.noreply.github.com>
2022-12-26 15:39:41 +0530

    single product change array into object

--- src/models/user/subscription.model.js
+++ src/models/user/subscription.model.js
@@ -138,12 +138,12 @@ export const single_subscription = async (userId, sub_id) => {
         "sub.subscribe_type_id"
       )
       .join("user_address", "user_address.id", "=", "sub.user_address_id")
-      .where({ "sub.user_id": userId});
-
+      .where({ "sub.user_id": userId, "sub.id": sub_id });
+      
       // const query1 = await knex("additional_orders").select("date")
       // console.log(query1[0].date)
       // // .moment(query1[0].additional_orders.date).format('YYYY-MM-DD');
-
+        console.log(products)
       const query = await knex("additional_orders")
       .select(
         "additional_orders.id",
@@ -158,8 +158,8 @@ export const single_subscription = async (userId, sub_id) => {
       )
       .join("products", "products.id", "=", "additional_orders.id")
       .join("unit_types", "unit_types.id", "=", "products.unit_type_id")
-      .join("user_address", "user_address.id", "=", "additional_orders.id")
-      // .where({ "add_on_orders.user_id": userId, "add_on_orders.id": add_on_orders.user_id });
+      .join("user_address", "user_address.id", "=", "additional_orders.subscription_id")
+      .where({ "additional_orders.user_id": userId });
 
     if (products.length === 0) {
       return { status: false, message: "No Subscription Found" };
