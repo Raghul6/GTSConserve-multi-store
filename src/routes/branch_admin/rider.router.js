78b4232bd0487d34747469959e559785ffcb78fd
ahamed hussain <ahamedhussain200208@gmail.com>
2023-01-10 10:49:38 +0530

    rider chnage password updated and alert message updated

--- src/routes/branch_admin/rider.router.js
+++ src/routes/branch_admin/rider.router.js
@@ -1,6 +1,6 @@
 import express  from 'express';
 
-import { getRiders,createRider,updateRider,updateRiderStatus} from '../../controllers/branch_admin/rider/rider.controller';
+import { getRiders,createRider,updateRider,updateRiderStatus,changePasswordRider} from '../../controllers/branch_admin/rider/rider.controller';
 
 
 const riderRouter = express.Router({
@@ -13,5 +13,8 @@ riderRouter.get('/get_rider',getRiders)
 riderRouter.post('/create_rider',createRider) 
 riderRouter.post('/update_rider',updateRider) 
 riderRouter.post('/update_rider_status',updateRiderStatus) 
+
+
+riderRouter.post('/change_password_rider',changePasswordRider) 
 
 export default riderRouter
