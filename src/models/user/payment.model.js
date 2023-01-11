5b21e15c23f083d87e7774dadc39473ff94ae7de
bhoobalan2022 <110883166+bhoobalan2022@users.noreply.github.com>
2023-01-11 11:44:21 +0530

    bill api

--- src/models/user/payment.model.js
+++ src/models/user/payment.model.js
@@ -1,17 +1,21 @@
-// import responseCode from "../../constants/responseCode";
+import responseCode from "../../constants/responseCode";
 
-// import knex from "../../services/db.service";
+import knex from "../../services/db.service";
 
-// export const PaymentMethod = async (order_id) => {
-//     const payment = await knex('bill_history_details').insert({razorpay_bill_id:})
-//     // knex.select('key','value').from('app_settings')
+export const getPayment = async (amount,order_id, userId) => {
+    const payment = await knex('bill_history').select('bill_no','user_id')
+    // .where({"bill_history.user_id":userId})
+    .where({"bill_history.bill_no":order_id, "bill_history.user_id":userId,"bill_history.grand_total":amount})
+    // knex.select('key','value').from('app_settings')
+
+    console.log(payment)
     
-//     try {
-//     return { status: responseCode.SUCCESS, body: appSetting }
-//     } catch (error) {
-//       return { status: responseCode.FAILURE.INTERNAL_SERVER_ERROR, error}
-//     }
-//   }
+    try {
+    return { status: responseCode.SUCCESS, body: payment }
+    } catch (error) {
+      return { status: responseCode.FAILURE.INTERNAL_SERVER_ERROR, error}
+    }
+  }
 
 
   
